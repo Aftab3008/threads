e949aeeb0e733fb3b6526b5dc48919b02051c135
Aftab3008 <shaikaftab005@mail.com>
2024-05-16 15:09:41 +0530

    Communities implementation

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,36 +1,51 @@
 import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
 interface Props {
-    currentUserId: string;
-    accountId: string;
-    accountType: string;
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
 }
 
-export default async function ThreadsTab({currentUserId, accountId, accountType}:Props) {
-  let result=await fetchUserPosts(accountId);
-  if(!result){
+export default async function ThreadsTab({
+  currentUserId,
+  accountId,
+  accountType,
+}: Props) {
+  let result: any;
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
+  if (!result) {
     redirect("/");
   }
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {result.threads.map((thread:any)=>(
-        <ThreadCard 
-        key={thread._id} 
-        id={thread._id}
-        currentUserId={currentUserId}
-        parentId={thread.parentId}
-        content={thread.text}
-        author={accountType==="User"?
-            {name:result.name, id:result.id, image:result.image}:
-            {name:thread.author.name, id:thread.author.id, image:thread.author.image}
-        }
-        community={thread.community}
-        comments={thread.children}
-        createdAt={thread.createdAt}
+      {result.threads.map((thread: any) => (
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={currentUserId}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={
+            accountType === "User"
+              ? { name: result.name, id: result.id, image: result.image }
+              : {
+                  name: thread.author.name,
+                  id: thread.author.id,
+                  image: thread.author.image,
+                }
+          }
+          community={thread.community}
+          comments={thread.children}
+          createdAt={thread.createdAt}
         />
       ))}
     </section>
-  )
+  );
 }
